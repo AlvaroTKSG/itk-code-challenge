11d6975d5f925f54b01525fa315aac2df04fb2e1
AlvaroTKSG <alvaro.sanchez@theksquaregroup.com>
2023-05-19 16:54:58 -0500

    entrega alvaro 2

--- exercise.js
+++ exercise.js
@@ -1,7 +1,7 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const func = (c) => {
     /* Only make changes below this comment */
-    return 9
+    return 4
     /* Only make changes below this comment */
 }
 
