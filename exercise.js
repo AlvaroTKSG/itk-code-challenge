679f606efc40473516b366c398eeac00c7810271
gstr-dsm <gstr.dsm@gmail.com>
2023-05-19 16:41:22 -0600

    Comentario 01

--- exercise.js
+++ exercise.js
@@ -1,6 +1,7 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const func = (c) => {
     /* Only make changes below this comment */
+    // Recurso mandado por Stark Industries
     return 4
     /* Only make changes below this comment */
 }
