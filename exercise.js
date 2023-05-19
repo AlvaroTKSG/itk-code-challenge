9c17e3051d9babba6003ecc15e503fcf8d75cf78
gstr-dsm <gstr.dsm@gmail.com>
2023-05-19 16:10:50 -0600

    cambio 01

--- exercise.js
+++ exercise.js
@@ -1,7 +1,7 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const func = (c) => {
     /* Only make changes below this comment */
-    
+    return 3
     /* Only make changes below this comment */
 }
 
